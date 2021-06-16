7f7ef8bed35a074eb638090e7b25da068691b052
xiaolong Hao <2072451919@qq.com>
2021-06-16 10:40:29 +0800

    创建房间

--- src/service/messageDispacher.ts
+++ src/service/messageDispacher.ts
@@ -1,4 +1,5 @@
-import { customObjT } from './types'
+import { customObjT, GameMapT, RoomT, WsConnector } from './types'
+import { getRandomString, queryKeyExist } from './tools'
 
 function dispatcher(type: string): Function {
   let handler: Function | null = null
@@ -10,8 +11,15 @@ function dispatcher(type: string): Function {
   return handler || function () { }
 }
 
-function createRoom(content: customObjT) {
-  console.log('create')
+function createRoom(content: customObjT, user: WsConnector) {
+  let id: string = getRandomString()
+  const roomMap = user.gameMap.roomMap
+  while (queryKeyExist(roomMap, id)) {
+    id = getRandomString()
+  }
+  const room: RoomT = Object.assign({ id }, (content as RoomT))
+  roomMap[id] = room
+  console.log(user.id, user.token)
 }
 
 export default dispatcher
