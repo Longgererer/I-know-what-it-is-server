70d0550c54ee9b48ead8222a5bd71a188729b2d3
xiaolong Hao <2072451919@qq.com>
2021-06-21 22:19:24 +0800

    创建房间分发者

--- build/service/messageDispacher.js
+++ build/service/messageDispacher.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const tools_1 = require("./tools");
 function dispatcher(type) {
     let handler = null;
     switch (type) {
@@ -9,7 +10,14 @@ function dispatcher(type) {
     }
     return handler || function () { };
 }
-function createRoom(content) {
-    console.log('create');
+function createRoom(content, user) {
+    let id = tools_1.getRandomString();
+    const roomMap = user.gameMap.roomMap;
+    while (tools_1.queryKeyExist(roomMap, id)) {
+        id = tools_1.getRandomString();
+    }
+    const room = Object.assign({ id }, content);
+    roomMap[id] = room;
+    console.log(user.id, user.token);
 }
 exports.default = dispatcher;
